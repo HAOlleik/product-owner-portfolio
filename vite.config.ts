a3fa84bbfd374676e48e8c35963864025ffffa2f
Hussein Olleik <hussein.olleik5@gmail.com>
2026-02-23 06:32:08 -0500

    Fixed deployment for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/product-owner-portfolio/",
+  // base: "/product-owner-portfolio/", // GitHub Domain
+  base: "/", // Custome Domain
 });
